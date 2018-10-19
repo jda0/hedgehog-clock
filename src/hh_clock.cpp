f19ed08da3dc73d831e8a7ff40a4a786a4106f27
James D <da_j@live.co.uk>
2018-10-19 14:19:41 +0100

    Add clarity on backticks in, and number, TODOs

--- src/hh_clock.cpp
+++ src/hh_clock.cpp
@@ -12,7 +12,9 @@
 //
 // All todos (except making your hedgehog dance) can be done in one line each, 
 // and you do not need to remove anything. Every TODO needs to be completed 
-// for this project to work fully.
+// for this project to work fully. If a TODO mentions something in between
+// backticks that you need to copy, eg `*copythis`, don't copy the backticks
+// themselves!
 //
 // Some todos also require you to look in other files.
 
@@ -21,14 +23,14 @@ const unsigned long secsPerDisplayUpdate = 10;
 const unsigned long secsPerNtcUpdate = 10 * 60;
 
 void setup() {
-  // TODO: start motors. This line is the same as one in Challenge 2.
+  // TODO: start motors. This line is the same as one in Challenge 2. [1]
 
   display.begin(SSD1306_SWITCHCAPVCC); // Start talking to our display ( hello, display :D ).
   display.setTextColor(WHITE);
   display.setTextWrap(false);
 
   wifi = new HHWifi("ssid", "password"); // TODO: Fill these with the SSID and passphrase of the 
-                                         //  network your hedgehog will connect to.
+                                         //  network your hedgehog will connect to. [2]
   ntp = new HHNtp("pool.ntp.org");
 
   Serial.begin(9600); //debug
@@ -66,7 +68,7 @@ void setup() {
                          // We made the decision to start the HHSchema with two characters to make
                          //  sure the information will make sense.
                          // As `data` is a pointer, we access the fields (like spaces on a form) by
-                         //  pointing to them with an arrow.
+                         //  pointing to them with an arrow. [3]
 
     if (wifi->visualConnect(display) != WL_CONNECTED) {
       while (1) { delay(1); } // not connected; do nothing
@@ -104,7 +106,7 @@ void setup() {
     }
 
     face->ip = wifi->localIP();
-    // TODO: show FACE_NTPCONNECT (the same way we showed FACE_WIFICONNECT in HINT1)
+    // TODO: show FACE_NTPCONNECT (the same way we showed FACE_WIFICONNECT in HINT1) [4]
 
     unsigned long tempEpoch = ntp->connect(); // HINT2
     if (ntp->isOnline()) {
@@ -113,7 +115,7 @@ void setup() {
         alarmRang = true; // HINT4                     // ...prevent it going off for now!
       }
     } else {
-      // TODO: show FACE_NTPOFFLINE (the same way we showed FACE_WIFICONNECT in HINT1)
+      // TODO: show FACE_NTPOFFLINE (the same way we showed FACE_WIFICONNECT in HINT1) [5]
       delay(secsPerDisplayUpdate * 1000); // HINT3
     }
   }
@@ -134,10 +136,13 @@ void loop() {
     _displayi = secsPerDisplayUpdate;
 
     if ((data->epoch % 86400) / 60 == data->alarm && !alarmRang) {
-      // TODO: set alarmRang to true
+      // TODO: set alarmRang to true [6]
       
       // TODO: dance pony dance! Just like you did in Challenge 2, give your hedgehog some
-      //  dancemoves to show off when the alarm goes off!
+      //  dancemoves to show off when the alarm goes off! You'll need to be really clever
+      //  here - moves before the while statement will happen once, and moves inside the curly
+      //  brackets (called braces) will loop forever - but be careful with the delays!
+      // Remember, you need to use Motor.stop() if you want your hedgehog to stop moving! [6]
       Motor.right();
       while (1) {
         face->alarmFace(display, data->epoch); // strobe display
@@ -145,31 +150,31 @@ void loop() {
       }
       // END TODO: dance pony dance!
     } else if (ntp->getLastTime() + secsPerNtcUpdate < data->epoch) {
-      // TODO: show FACE_NTPCONNECT (the same way we showed FACE_WIFICONNECT in HINT1)
+      // TODO: show FACE_NTPCONNECT (the same way we showed FACE_WIFICONNECT in HINT1) [7]
       unsigned long tempEpoch = 0; // TODO: replace 0 with something NTP related...
                                    // ...maybe see line HINT2?
       if (0) { // TODO: replace 0 with something that tells us whether NTP is online...
-               // ..maybe see the line after HINT2?
-        // TODO: copy our temporary time to our data. Maybe see two lines after HINT2?
+               // ..maybe see the line after HINT2? [8]
+        // TODO: copy our temporary time to our data. Maybe see two lines after HINT2? [9]
         // TODO: Write our data to storage, using a function inside `HHPersistence`. You'll need to
-        //  put `*data` in the brackets, to send the information `data` points to.
+        //  put `*data` in the brackets, to send the information `data` points to. [10]
       } else {
         face->show(display, HHClockFace::Face::FACE_NTPOFFLINE, data->epoch);
-        // TODO: wait for the next display update using `delay`. Maybe see HINT3?
+        // TODO: wait for the next display update using `delay`. Maybe see HINT3? [11]
       }
     } else {
       face->next(display, data->epoch);
 
       if ((data->epoch % 86400) / 60 > data->alarm) {
         bool a = false; // TODO: reset the alarm by changing `bool a` to something else...
-                        // ...that maybe on line HINT4?
+                        // ...that maybe on line HINT4? [12]
       }
     }
   }
 
   // TODO: handle a client to our server. You'll need to look in `../src/hh_server.h` for the right
   //  function, which takes no arguments (has empty brackets after the name), and is in the 
-  //  `HHServer` namespace.
+  //  `HHServer` namespace. [13]
 
 }
 
